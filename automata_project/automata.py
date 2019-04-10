ca5f9aa70965b9938ae7310bfaf89362ddcd1cb5
Lee_Stack <leestackcz@gmail.com>
2019-04-10 08:45:58 +0200

    some minor changes

--- automata_project/automata.py
+++ automata_project/automata.py
@@ -1,19 +1,23 @@
 import itertools
-import json
+# import json
+
 
 def load_function(input_file):
     comands = {}
     with open(input_file, mode='r') as inp:
         for line in inp:
-            l = line.split()
-            comands[l[0]] = (l[1], l[2])
+            word = line.split()
+            comands[word[0]] = (word[1], word[2])
     return comands
+
 
 def is_in_alphabet(ch):
     return ch == '0' or ch == '1'
 
+
 def is_accepting_state(ch):
     return ch == '1'
+
 
 def iterate(input_file, comands):
     state = '0'
@@ -22,28 +26,28 @@ def iterate(input_file, comands):
             for char in line:
                 if is_in_alphabet(char):
                     state = comands[state][int(char)]
-                
+
     return state
+
 
 def iterateText(text, comands):
     state = '0'
     for char in text:
-        #print(f"{state} , {char} : {is_in_alphabet(char)}")
+        # print(f"{state} , {char} : {is_in_alphabet(char)}")
         if is_in_alphabet(char):
             state = comands[state][int(char)]
-        
     return state
 
 
 COMANDS = load_function("text.in")
 print(COMANDS)
-#STATE = iterate("input.in", COMANDS)
-#print(iterateText("011010", COMANDS))
+# STATE = iterate("input.in", COMANDS)
+# print(iterateText("011010", COMANDS))
 
 
-correct_states= []
+correct_states = []
 
-words = ["".join(x) for i in range(1,9) for x in itertools.product("01", repeat=i) ]
+words = ["".join(x) for i in range(1, 9) for x in itertools.product("01", repeat=i)]
 
 for item in words:
     STATE = iterateText(item, COMANDS)
@@ -55,5 +59,3 @@ print(correct_states.__len__())
 print(words.__len__())
 
 print(correct_states)
-
-
