75aaed791685ae5e021590cf11817ba478f80284
Lee_Stack <leestackcz@gmail.com>
2019-04-12 16:14:31 +0200

    fixed mrange

--- 06/range.py
+++ 06/range.py
@@ -1,5 +1,5 @@
 def mrange(*args):
-    beg, step = 0, 1
+    beg, step, limit = 0, 1, 0
     if args.__len__() == 1:
         limit = args[0]
     if args.__len__() >= 2:
@@ -7,8 +7,27 @@ def mrange(*args):
         beg = args[0]
     if args.__len__() == 3:
         step = args[2]
-    if args.__len__() > 3:
+    if args.__len__() > 3 | args.__len__() < 1:
         raise Exception("argument error")
-    while beg < limit:
-        yield beg
-        beg =+ step
+    if step > 0:
+        while beg < limit:
+            yield beg
+            beg += step
+    else:
+        while beg > limit:
+            yield beg
+            beg += step
+
+
+# testing data
+lis = list(i for i in mrange(10))
+print(lis)
+
+lis = list(i for i in mrange(1, 10))
+print(lis)
+
+lis = list(i for i in mrange(10, 1, -1))
+print(lis)
+
+lis = list(i for i in range(10, 1, -1))
+print(lis)
