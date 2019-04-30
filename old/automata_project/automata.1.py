900461e6ba3410cf2be8586d491ac7dfe29542f0
Lee_Stack <leestackcz@gmail.com>
2019-04-30 21:23:40 +0200

    ..

--- old/automata_project/automata.1.py
+++ old/automata_project/automata.1.py
@@ -151,7 +151,6 @@ aut1 = automaton("data.json")
 
 aut1.replace_instructions("st0", "b", "st0", "MVR")
 aut1.add_instruction("st0", "b", "st0", "MVR")
-aut1.clear()
 aut1.print_instructions()
 aut1.save_instructions("data.1.json")
 print(aut1.is_deterministic())
