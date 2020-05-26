67661365868c0eaa855623db6cf961139b4ecb7e
Listonoh <LeeStackcz@gmail.com>
2020-05-26 19:45:58 +0200

    ok

---
+++ katas/lkl.py
@@ -0,0 +1,15 @@
+def solve(s):
+    a = s.split(["a","e","i","o","u"])
+    a = s.replace
+    print(a)
+    #max = max( sum[chr(a) for a in word ] for word in a)
+    print(max)
+
+
+print(solve("zodiac"))
+print(solve("chruschtschov"))
+print(solve("khrushchev"))
+print(solve("strength"))
+print(solve("catchphrase"))
+print(solve("twelfthstreet"))
+print(solve("mischtschenkoana"))
